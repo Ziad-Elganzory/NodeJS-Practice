2c82d86a831561b5757a3959335e5f4e62843279
Ziad-Elganzory <ziadmohamed770@outlook.com>
2025-04-21 21:02:59 +0200

    Get , Post , Patch Methods Implemented

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import fs, {promises as fsPromises, stat} from 'fs';
+import fs, {promises as fsPromises} from 'fs';
 import path from 'path';
 import { generateFakeData } from './utils/fakeData';
 import { IProduct } from './interfaces/IProducts';
@@ -10,13 +10,81 @@ const productsPath = path.join(__dirname,'data','products.json');
 
 const fakeProducts = generateFakeData();
 
+/**
+ * 
+ * To parse a custom header
+ * 
+ *  */ 
 
-app.get('/', (req, res) => {
+// app.use(express.json({
+//     type: "custom/type",
+// }))
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
     res.send('Hello World');
 });
 
-// Endpoint to fetch products from JSON file
-app.get('/products',async(req, res) => {
+/**
+ * 
+ * Endpoint to fetch products from JSON file
+ *  
+ * */
+
+// Create Product
+app.post('/products',async(req: Request, res: Response) => {
+    try{
+        const requestBody = req.body;
+        const {title, description} = requestBody;
+        try {
+            await fsPromises.access(productsPath);
+        } catch {
+            // Create the file if it doesn't exist
+            await fsPromises.writeFile(productsPath, JSON.stringify({ products: [] }, null, 2));
+        }
+        const productsData = await fsPromises.readFile(productsPath,'utf8');
+        const productsJSON : {products: {id: number, title: string, description: string}[]} = JSON.parse(productsData);
+
+        if(!title || !description){
+            res.status(400).send({
+                status:400,
+                message:'Product ID, Title and Description are required'
+            });
+            return;
+        }
+        if(typeof title !== 'string' || typeof description !== 'string'){
+            res.status(400).send({
+                status:400,
+                message:'Product ID should be a number and Title and Description should be strings'
+            });
+            return;
+        }
+        const newProduct = {
+            id: productsJSON.products.length + 1,
+            title: title,
+            description: description
+        };
+        productsJSON.products.push(newProduct);
+        const updatedData = JSON.stringify(productsJSON, null, 2);
+        await fsPromises.writeFile(productsPath,updatedData,{flag:"w"});
+        res.status(201).send({
+            status:201,
+            message:'Product Created Successfully',
+            body: requestBody
+        })
+    } catch(err){
+        console.error('File does not exist:', productsPath);
+        res.status(404).send({
+            status:404,
+            message:'File not found'
+        });
+        return;
+    }
+});
+
+// Get Products
+app.get('/products',async(req: Request, res: Response) => {
     try{
         await fsPromises.access(productsPath);
         const data = await fsPromises.readFile(productsPath,'utf8');
@@ -30,7 +98,7 @@ app.get('/products',async(req, res) => {
         }
         res.setHeader('Content-Type', 'application/json');
         res.send({
-            status:200,
+            status:201,
             message:'Product Fetched Successfully',
             products: productsData.products
         });    }
@@ -44,6 +112,7 @@ app.get('/products',async(req, res) => {
     }
 });
 
+// Get Product by ID
 app.get('/products/:id',async(req: Request, res: Response) => {
     const productID = +req.params.id;
     if(isNaN(productID)){
@@ -63,7 +132,7 @@ app.get('/products/:id',async(req: Request, res: Response) => {
             return;
         }
         res.setHeader('Content-Type', 'application/json');
-        res.send({
+        res.status(200).send({
             status:200,
             message:'Product Fetched Successfully',
             product: product
@@ -78,7 +147,42 @@ app.get('/products/:id',async(req: Request, res: Response) => {
     }
 });
 
+
+
+/**
+ * 
+ * Endpoint to Fetch fake products from Faker js
+ * 
+ */
+
 // Endpoint to generate fake products
+app.post('/fake-products',async(req: Request, res: Response) => {
+    try{
+        const requestBody = req.body;
+        const {title, price, description} = requestBody;
+        const newProduct = {
+            id: fakeProducts.length + 1,
+            title: title,
+            price: price,
+            description: description
+        };
+        fakeProducts.push(newProduct);
+        res.status(201).send({
+            status:201,
+            message:'Fake Product Created Successfully',
+            body: requestBody
+        });
+    } catch(err){
+        res.status(500).send({
+            status:500,
+            message:'Internal Server Error',
+            error:err
+        });
+        return;
+    }
+})
+
+// Endpoint to get fake products with filter query fetaure
 app.get('/fake-products',(req: Request, res: Response) => {
     // Filter By Query, keyof IProduct
     const filterQuery = req.query.filter as string;
@@ -98,7 +202,7 @@ app.get('/fake-products',(req: Request, res: Response) => {
             return {id: product.id ,...filteredProduct};
         })
         res.send({
-            status:200,
+            status:201,
             message:'Fake Products Fetched Successfully',
             products: filteredProducts
         });
@@ -113,6 +217,7 @@ app.get('/fake-products',(req: Request, res: Response) => {
 
 });
 
+// Endpoint to get fake product by ID
 app.get('/fake-products/:id',(req: Request, res: Response) => {
     const productID = +req.params.id;
     if(isNaN(productID)){
@@ -138,6 +243,44 @@ app.get('/fake-products/:id',(req: Request, res: Response) => {
             message:'Fake Product Fetched Successfully',
             product: product
         });
+    } catch(err){
+        res.status(500).send({
+            status:500,
+            message:'Internal Server Error',
+            error:err
+        });
+        return;
+    }
+});
+
+// Endpoint to Update a certain fake product
+app.patch('/fake-products/:id',(req: Request, res: Response) => {
+    const productID = +req.params.id;
+    if(isNaN(productID)){
+        res.status(400).send({
+            status:400,
+            message:'Invalid product ID'
+        });
+        return;
+    }
+
+    try{
+        const productIndex: number | undefined = fakeProducts.findIndex(product => product.id === productID);
+        if(productIndex === -1){
+            res.status(404).send({
+                status:404,
+                message:`Product ID: ${productID} Not Found`
+            });
+            return;
+        }
+        const productBody = req.body;
+        fakeProducts[productIndex] = { ...fakeProducts[productIndex], ...productBody };
+        res.status(200).send({
+            status:200,
+            message:'Fake Product Updated Successfully',
+            product: fakeProducts[productIndex]
+        });
+        
     } catch(err){
         res.status(500).send({
             status:500,
